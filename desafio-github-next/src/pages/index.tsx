ec9aec76ddfc88435536d5f0cec321c059783dc2
ianvictor <ianvictorandrade@hotmail.com>
2024-02-02 09:40:33 -0400

    fix

--- desafio-github-next/src/pages/index.tsx
+++ desafio-github-next/src/pages/index.tsx
@@ -1,10 +1,13 @@
 import Principal from "../components/principal/paginaPrincipal";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
   
     export default function Home() {
         return (
             <div className="flex bg-[#F1F1F6] items-center justify-center h-screen">
                 <Principal />
+                <ToastContainer />
             </div>
         )
     }
