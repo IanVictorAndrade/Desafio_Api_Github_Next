1f3bdbabe222b029b9d658049a50303b34356d75
ianvictor <ianvictorandrade@hotmail.com>
2024-02-06 12:42:33 -0400

    corrigido

--- desafio-github-next/src/components/userPage/userPage.tsx
+++ desafio-github-next/src/components/userPage/userPage.tsx
@@ -5,13 +5,12 @@ import { toast } from "react-toastify";
 import github from "@/public/github.svg";
 import Image from "next/image";
 
-
 interface Repo {
     name: string;
     html_url: string;
     description: string;
     user_name: string;
-    avatar: string;
+    avatar_url: string; // Corrigido o tipo para string
 }
 
 export default function UserPage() {
@@ -28,7 +27,7 @@ export default function UserPage() {
                             html_url: repo.html_url,
                             description: repo.description,
                             user_name: repo.owner.login,
-                            avatar: repo.owner.avatar_url
+                            avatar_url: repo.owner.avatar_url
                         };
                     });
                     setRepos(reposData);
@@ -43,10 +42,12 @@ export default function UserPage() {
         <>
             <div className="flex flex-col justify-center items-center gap-4">
                 <Image src={github} width={118} height={32} className="mt-16" alt="" />
+                {repos.length > 0 && (
                     <div className="flex justify-center items-center gap-3">
-                        <img src={repos[0].avatar} className="w-[3.75rem] h-[3.75rem] rounded-3xl" alt="foto do usuário" />
+                        <img src={repos[0].avatar_url} className="w-[3.75rem] h-[3.75rem] rounded-3xl" alt="foto do usuário" />
                         <p className="text-2xl font-bold">{repos[0].user_name}</p>
                     </div>
+                )}
             </div>
             <section className="grid grid-cols-3 gap-[2rem] justify-center items-center w-[75%] m-auto mb-[30%]">
                 {repos.map((repo) => (
